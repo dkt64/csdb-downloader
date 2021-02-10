7860ecd1cbce61fa9969f931cedfdde65c512e6a
DKT <b.apanasewicz@gmail.com>
2021-02-10 12:36:55 +0100

    first release

--- build.bat
+++ build.bat
@@ -1,18 +1,18 @@
 set GOOS=windows
 set GOARCH=amd64
-go build -o bin/win64/csdb-downloader-win64.exe
+go build -o bin/csdb-downloader-win64/csdb-downloader-win64.exe
 set GOOS=windows
 set GOARCH=386
-go build -o bin/win32/csdb-downloader-win32.exe
+go build -o bin/csdb-downloader-win32/csdb-downloader-win32.exe
 set GOOS=linux
 set GOARCH=amd64
-go build -o bin/lin64/csdb-downloader-amd64
+go build -o bin/csdb-downloader-linux_amd64/csdb-downloader-linux-amd64
 set GOOS=linux
 set GOARCH=arm64
-go build -o bin/arm64/csdb-downloader-arm64
+go build -o bin/csdb-downloader-linux_arm64/csdb-downloader-linux-arm64
 set GOOS=darwin
 set GOARCH=amd64
-go build -o bin/mac_amd64/csdb-downloader-mac-amd64
+go build -o bin/csdb-downloader-mac_amd64/csdb-downloader-mac-amd64
 set GOOS=darwin
 set GOARCH=arm64
-go build -o bin/mac_arm64/csdb-downloader-mac-arm64
+go build -o bin/csdb-downloader-mac_arm64/csdb-downloader-mac-arm64
